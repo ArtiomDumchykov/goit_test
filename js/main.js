2cd02eaaf8909752e1df662a0790f3c810c5ccf0
Dumchykov Artem <dum4ickov6102@gmail.com>
2022-07-01 13:41:53 +0300

    test_app_v_1.3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 (() => {
     const burgerToggle = document.querySelector(".nav__burger");
-    console.log(111)
+    // console.log(111)
 
     burgerToggle.addEventListener("click", (event) => {
         event.preventDefault()
 
         console.log(123);
-        burgerToggle.classList.toggle('active')
-    })
+        // burgerToggle.classList.toggle('active');
+        !burgerToggle.classList.contains('active') ?
+                // console.log(1)
+                burgerToggle.classList.add("active") 
+            :
+                // console.log(2);
+                burgerToggle.classList.remove("active")
+    })         
 })()
