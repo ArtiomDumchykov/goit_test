62d5b8400e53f8e6079ff7f029942924bde4cdfe
Dumchykov Artem <dum4ickov6102@gmail.com>
2022-07-01 22:51:38 +0300

    test_app_v_1_6

--- js/main.js
+++ js/main.js
@@ -1,22 +1,80 @@
-(() => {
+(window.addEventListener(('DOMContentLoaded'), () => {
     const burgerToggle = document.querySelector(".nav__burger");
     const navToggle = document.querySelector('.nav');
 
+
+    const intro = document.querySelector(".intro");
+    const header = document.querySelector(".header");
+
+    const actionEventListenerUtils = {
+        addAction: "add",
+        deleteAction: "remove"
+      }
+
+
     const fas_items = document.querySelectorAll(".fas__item").forEach(x => x.hidden = true)
+
+    // Smooth scroll to link
+
+    const linkList = document = document.querySelectorAll(".nav-link[href*='#']");
+    
+    linkList.forEach((x) => {
+        x.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const {addAction, deleteAction} = actionEventListenerUtils;
+
+            const linkHref = x.getAttribute("href");
+            console.log(linkHref);
+            document.querySelector(linkHref).scrollIntoView({
+                behavior: "smooth",
+                block: 'start',
+            })
+
+            activeToggle(burgerToggle, deleteAction, "active");
+            activeToggle(navToggle, deleteAction, "active");
+        })
+    })
+
+    // Toggle
+
+    
+
+    function activeToggle(...args) {
+        const [element, action, className] = args; 
+
+        element.classList[action](className);
+    }
+
 
     burgerToggle.addEventListener("click", (event) => {
         event.preventDefault()
+        const {addAction, deleteAction} = actionEventListenerUtils
 
 
         !burgerToggle.classList.contains('active') ?
-                // console.log(1)
-                (burgerToggle.classList.add("active"),
-                navToggle.classList.add("active")) 
+
+                (activeToggle(burgerToggle, addAction, "active"),
+                    activeToggle(navToggle, addAction, "active"))
             :
-                // console.log(2);
-                (burgerToggle.classList.remove("active"),
-                navToggle.classList.remove("active"))
+                (activeToggle(burgerToggle, deleteAction, "active"),
+                    activeToggle(navToggle, deleteAction, "active"))
     })  
     
+
+    // FixedHeader
+
+    function fixedHeader() {
+        const {addAction, deleteAction} = actionEventListenerUtils;
+
+        if (window.scrollY >= intro.offsetHeight * 0.915) {
+            activeToggle(header, addAction, "header__fixed")
+        } else {
+            activeToggle(header, deleteAction, "header__fixed")
+        }
+
+    }
+
+    window.addEventListener('scroll', fixedHeader);
     
-})()
+}))()
