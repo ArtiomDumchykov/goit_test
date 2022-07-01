0a69c6594ac0fc77676f791d293116db7ed104d6
Dumchykov Artem <dum4ickov6102@gmail.com>
2022-07-01 20:51:54 +0300

    test_app_1.4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 (() => {
     const burgerToggle = document.querySelector(".nav__burger");
-    // console.log(111)
+    const navToggle = document.querySelector('.nav');
+
+    const fas_items = document.querySelectorAll(".fas__item").forEach(x => x.hidden = true)
 
     burgerToggle.addEventListener("click", (event) => {
         event.preventDefault()
 
-        console.log(123);
-        // burgerToggle.classList.toggle('active');
+
         !burgerToggle.classList.contains('active') ?
                 // console.log(1)
-                burgerToggle.classList.add("active") 
+                (burgerToggle.classList.add("active"),
+                navToggle.classList.add("active")) 
             :
                 // console.log(2);
-                burgerToggle.classList.remove("active")
-    })         
+                (burgerToggle.classList.remove("active"),
+                navToggle.classList.remove("active"))
+    })  
+    
+    
 })()
